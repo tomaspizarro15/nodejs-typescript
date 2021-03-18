0b3e0905be3b0a87ed50af63804689972f254137
tomaspizarro15 <61084368+tomaspizarro15@users.noreply.github.com>
2021-03-18 10:43:50 -0300

    enviroment-changes

--- bin/api.ts
+++ bin/api.ts
@@ -1,9 +1,14 @@
-import app from './../app'
-
-app.use(() => {
-
+import app from './../app';
+import https from 'http';
+import config from 'config'
+const port : number = config.get('server.port')
+app.use('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Helloworld",
+        status: 200,
+    })
 })
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening at", process.env.PORT)
+app.listen(port , ()=> {
+    console.log(port)
 })
